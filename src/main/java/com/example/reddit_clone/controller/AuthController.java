a2aa8aba670c4fdd7633c462698cd60bac7e5ac0
philipdaquin <philipdaquin20@gmail.com>
2022-09-30 12:51:57 +0800

    added auth controller ,verification token/ repo and auth service

---
+++ src/main/java/com/example/reddit_clone/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.example.reddit_clone.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/signup")
+    public void signUp(@RequestBody RegisterRequest registerReq) {
+        //todo!
+    }
+}
---
+++ src/main/java/com/example/reddit_clone/models/VerificationToken.java
@@ -0,0 +1,32 @@
+package com.example.reddit_clone.models;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token")
+public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+    private Instant expiryDate;
+}

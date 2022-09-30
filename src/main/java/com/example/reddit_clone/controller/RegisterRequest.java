78e60e50e61b62ab218c50fd6dc3d9d242412bef
philipdaquin <philipdaquin20@gmail.com>
2022-09-30 12:52:15 +0800

    based on the previous commit

---
+++ src/main/java/com/example/reddit_clone/controller/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.example.reddit_clone.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/example/reddit_clone/service/AuthService.java
@@ -0,0 +1,65 @@
+package com.example.reddit_clone.service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
+import org.aspectj.internal.lang.annotation.ajcDeclareAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.reddit_clone.controller.RegisterRequest;
+import com.example.reddit_clone.models.User;
+import com.example.reddit_clone.models.VerificationToken;
+import com.example.reddit_clone.repository.UserRepository;
+import com.example.reddit_clone.repository.VerificationRepo;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    /*
+     * Find me the matching object in the SecurityConfig 
+        @Autowired
+     */
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationRepo verificatioRepo;
+    /** 
+     * @Transactional 
+     * Spring creates proxies for all the classes annotated with . 
+     * either on the class or on any of the methods
+     */
+    @Transactional
+    public void signUp(RegisterRequest req) { 
+        User user = new User();
+
+        user.setUsername(req.getUsername());
+        user.setEmail(req.getEmail());
+        user.setPassword(passwordEncoder.encode(req.getPassword()));
+        user.setCreatedAt(Instant.now());
+        user.setEnabled(false);
+
+        // Save user 
+        userRepository.save(user);
+
+        generatedVerificationToken(user);
+    }
+
+    private String generatedVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        // Create  a new Verification token 
+        var verifiedToken = new VerificationToken();
+        verifiedToken.setToken(token);
+        verifiedToken.setUser(user);
+
+        verificatioRepo.save(verifiedToken);        
+        return token;
+    }
+
+}
+

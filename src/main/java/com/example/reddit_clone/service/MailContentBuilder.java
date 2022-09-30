79b7ae699cd8d904c5c9af9b9bb424e11f600a3e
philipdaquin <philipdaquin20@gmail.com>
2022-09-30 14:54:34 +0800

    created mail content builder and mail service

---
+++ src/main/java/com/example/reddit_clone/service/MailContentBuilder.java
@@ -0,0 +1,23 @@
+package com.example.reddit_clone.service;
+
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import lombok.AllArgsConstructor;
+
+@Service 
+@AllArgsConstructor
+public class MailContentBuilder {
+    private final TemplateEngine templateEngine;
+
+    /*
+     * Thymeleaf is a Java template engine for processing and creating 
+     * HTML, XML, JavaScript, CSS and text 
+     */
+    String build(String message)  {
+        Context context = new Context();
+        context.setVariable("message", message);
+        return templateEngine.process("mailTemplate", context);
+    }
+}
---
+++ src/main/java/com/example/reddit_clone/service/MailService.java
@@ -0,0 +1,46 @@
+package com.example.reddit_clone.service;
+
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.example.reddit_clone.exceptions.NotificationException;
+import com.example.reddit_clone.models.NotificationEmail;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class MailService {
+    
+    private final static Logger LOGGER = LoggerFactory
+        .getLogger(MailService.class);
+    
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    @Async
+    void sendMail(NotificationEmail email) { 
+        System.out.println("\n✅ MailService.sendMail()");
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            var messageHelper = new MimeMessageHelper(mimeMessage);
+                messageHelper.setFrom("reddit_clone@gmail.com");
+                messageHelper.setTo(email.getRecipient());
+                messageHelper.setSubject(email.getSubject());
+                messageHelper.setText(email.getBody());
+        };
+        try {
+            System.out.println("\n✅ Sending over Email!");
+            mailSender.send(messagePreparator);
+        } catch (MailException e) {
+            LOGGER.error("Failed to send email", e);
+            throw new NotificationException("Exception occurred when sending mail to " + email.getRecipient(), e);
+        }
+    }
+}

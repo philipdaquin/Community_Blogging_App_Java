2988301afa1e9826362af4c74c4773c5a45e770f
philipdaquin <philipdaquin20@gmail.com>
2022-10-01 12:11:31 +0800

    added auth manager to verify user

--- src/main/java/com/example/reddit_clone/config/SecurityConfig.java
+++ src/main/java/com/example/reddit_clone/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.example.reddit_clone.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.reddit_clone.service.UserDetailsServiceImpl;
 
 import lombok.AllArgsConstructor;
 
@@ -14,6 +20,14 @@ import lombok.AllArgsConstructor;
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+ 
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     public void configure(HttpSecurity http) throws Exception { 
         http.csrf()
@@ -34,6 +48,12 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() { 
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authManager) { 
+        // authManager.userDetailsService(userDetailsService)
+        //     .passwordEncoder(passwordEncoder());
     }
 
     
---
+++ src/main/java/com/example/reddit_clone/service/UserDetailsServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.reddit_clone.service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.reddit_clone.models.User;
+import com.example.reddit_clone.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+import static java.util.Collections.singletonList;
+
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOption = userRepository.findByUsername(username);
+
+        var user = userOption
+            .orElseThrow(() -> new UsernameNotFoundException("Unable to retrieve the user details"));
+        
+        // This is provided by the Spring Framework 
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), 
+            user.getPassword(), 
+            user.isEnabled(), 
+            true, 
+            true, 
+            true, 
+            getAuthorities("USER")
+        );
+    }
+
+    /**
+     * This will return the Object collections 
+     * @param role
+     * @return
+     */
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return singletonList(new SimpleGrantedAuthority(role));
+    }
+ 
+}

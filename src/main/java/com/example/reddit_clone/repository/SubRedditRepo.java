33b3dadb261abd82ac39afae2de099ecc315fa2f
philipdaquin <philipdaquin20@gmail.com>
2022-10-04 10:53:11 +0800

    added find by name api in database

--- src/main/java/com/example/reddit_clone/repository/SubRedditRepo.java
+++ src/main/java/com/example/reddit_clone/repository/SubRedditRepo.java
@@ -1,10 +1,14 @@
 package com.example.reddit_clone.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.reddit_clone.models.SubReddit;
 
 
 public interface SubRedditRepo extends JpaRepository<SubReddit, Long> {
-    
+
+    Optional<SubReddit> findByName(String subredditName);
+       
 }

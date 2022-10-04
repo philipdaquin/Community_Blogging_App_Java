19472792ef793b948e0d83bfcd1398e1edf7711d
philipdaquin <philipdaquin20@gmail.com>
2022-10-04 10:53:46 +0800

    added findbysubredditname and findbyuser in postRepositoru

--- src/main/java/com/example/reddit_clone/repository/PostRepository.java
+++ src/main/java/com/example/reddit_clone/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.example.reddit_clone.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.reddit_clone.models.Post;
+import com.example.reddit_clone.models.SubReddit;
+import com.example.reddit_clone.models.User;
 
 
 /*
@@ -15,5 +19,8 @@ import com.example.reddit_clone.models.Post;
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findBySubredditName(SubReddit subredditName);
+    List<Post> findByUser(User user);
 
 }
